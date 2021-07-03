ffecb81e3754dee9281355f52bd5e2128d543e8b
Pragya <pragyamishra10501@gmail.com>
2021-07-03 16:31:38 +0530

    Version 2.0 Added Frontend for all pages

--- Web Page Django/myapp/models.py
+++ Web Page Django/myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Employee(models.Model):
     eid = models.CharField(max_length = 20)
     ename = models.CharField(max_length=100)
+    eclass = models.CharField(max_length=100,null=True,blank=False)
     eemail = models.CharField(max_length=50)
     econtact = models.CharField(max_length = 30)
 
